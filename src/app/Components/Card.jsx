a12bef0c27b69d669d923071b6b2ee9f15a4414d
shahrazmirza <shahrazahmedmirza@live.com>
2024-07-30 11:54:57 +1000

    Commit

--- src/app/Components/Card.jsx
+++ src/app/Components/Card.jsx
@@ -15,10 +15,12 @@ function Card() {
   const [weatherData, setWeatherData] = useState(null);
   const [error, setError] = useState(null);
 
+  const API_KEY = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
+
   const getWeatherData = async (city) => {
     try {
       const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?appid=914cd4190a0cdd48e9112eb8afdbf306&units=metric&q=${city}`
+        `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&units=metric&q=${city}`
       );
 
       if (response.status === 404) {
@@ -73,7 +75,7 @@ function Card() {
         <>
           <Image
             src={
-              weatherData.weather && weatherData.weather[0].main === "Clouds"
+              weatherData.weather[0].main === "Clouds"
                 ? clouds
                 : weatherData.weather[0].main === "Clear"
                 ? clear
